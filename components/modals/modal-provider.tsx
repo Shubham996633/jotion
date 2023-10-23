f2757a5f242c03b01829173f117fbe2795561338
Shubham Maurya <65014926+Shubham996633@users.noreply.github.com>
2023-10-23 16:26:53 +0530

    update

--- components/modals/modal-provider.tsx
+++ components/modals/modal-provider.tsx
@@ -1,18 +1,25 @@
-"use client"
+"use client";
 
-import {useEffect, useState} from "react"
-import { SettingsModal } from "./settings-modal"
+import { useEffect, useState } from "react";
+
+import { SettingsModal } from "@/components/modals/settings-modal";
+import { CoverImageModal } from "@/components/modals/cover-image-modal";
+
 export const ModalProvider = () => {
-    const [isMounted, setIsMounted] = useState(false)
-    useEffect(() => {
-        setIsMounted(true)
-    
-    })
-    if(!isMounted) return null
-    
-    return(
-        <>
-            <SettingsModal/>
-        </>
-    )
-}
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+  
+  return (
+    <>
+      <SettingsModal />
+      <CoverImageModal />
+    </>
+  );
+};
